93498b408ba56ac01363d8d84cac13f363fb5cb4
Marco Castanho <marco.castanho@gmail.com>
2025-07-22 00:47:27 +0100

    make button pointer

--- src/components/ui/buttonVariants.ts
+++ src/components/ui/buttonVariants.ts
@@ -1,7 +1,7 @@
 import { cva } from "class-variance-authority";
 
 export const buttonVariants = cva(
-  "inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none",
+  "inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors cursor-pointer focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none",
   {
     variants: {
       variant: {
