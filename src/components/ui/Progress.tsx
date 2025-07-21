a38dc96ad3a07f2f9897f59ca74a2d70a038b947
Marco Castanho <marco.castanho@gmail.com>
2025-07-21 22:11:19 +0100

    fix progress bar styling

--- src/components/ui/Progress.tsx
+++ src/components/ui/Progress.tsx
@@ -6,10 +6,14 @@ interface ProgressProps {
 
 export const Progress = ({ value, className }: ProgressProps) => {
   const numericValue = typeof value === "string" ? parseFloat(value) : value;
+  const isComplete = numericValue >= 100;
 
   return (
-    <div className={`w-full bg-gray-200 rounded-full h-1.5 mb-4 dark:bg-gray-700 ${className}`}>
-      <div className={`bg-blue-600 h-1.5 rounded-full dark:bg-blue-500`} style={{ width: numericValue }} />
+    <div className={`w-full bg-gray-200 rounded-full h-1.5 mb-4 ${className}`}>
+      <div
+        className={`bg-blue-600 h-1.5 rounded-full  ${isComplete ? "bg-green-600" : "bg-yellow-500"}`}
+        style={{ width: `${Math.min(numericValue, 100)}%` }}
+      />
     </div>
   );
 };
