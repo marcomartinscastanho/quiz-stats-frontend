db0e3df02b81c3a2af69e6b2f199741e192b4bcd
Marco Castanho <marco.castanho@gmail.com>
2025-07-31 12:02:55 +0100

    fix part 2 ui

--- src/components/ui/TopicsCategories.tsx
+++ src/components/ui/TopicsCategories.tsx
@@ -21,14 +21,14 @@ export const TopicsCategories: React.FC<Props> = ({ title, topics, options, onCh
     <div className="flex-1 flex flex-col gap-1">
       <h3 className="font-semibold mb-2">{title}</h3>
       {topics.map((topic, idx) => (
-        <div key={topic.topic} className="flex flex-col gap-1 md:flex-row md:gap-4 md:items-center">
-          <label className="block font-medium">{topic.topic}</label>
+        <div key={topic.topic} className="flex flex-col gap-1 sm:flex-row sm:gap-4 sm:items-center">
+          <label className="flex-1 block font-medium">{topic.topic}</label>
           <Select
             isMulti
             options={options}
             value={topic.categories.map(c => ({ value: c.id, label: c.name }))}
             onChange={(selected: MultiValue<Option>) => updateFirstHalfCategories(idx, [...(selected || [])])}
-            className="react-select-container flex-1"
+            className="react-select-container flex-2"
             classNamePrefix="react-select"
             menuPlacement="auto"
           />
